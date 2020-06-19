7c55595de31729fb70ffada655231d6b1ec5d703
Lynn Yuan <lynnyuann@gmail.com>
2020-06-19 12:31:01 +1200

    defined action, reducer and store

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('from index.js')
+const redux = require('redux')
+const createStore = redux.createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+
+//an action is an object with a type property, 
+//action creater is a function returns an action
+function buyCake(){
+    return{
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+//reducer is a function takes state and aciton returns a new state
+//(previousState,action) => newState 
+const initialState = {
+    numOfCakes: 10
+}
+const reducer = (state = initialState, action) => {
+      switch (action.type) {
+        case BUY_CAKE: return {
+          ...state,
+          numOfCakes: state.numOfCakes - 1
+        }
+        default: return state
+      }
+    }
+    
+const store = createStore(reducer)
+console.log('initial state', store.getState())
+const unsubscribe = store.subscribe(()=>console.log('updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
